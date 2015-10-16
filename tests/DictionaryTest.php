2dd780eb2d5caccbe7a7d3d3fe6eeb1dc3f62649
andela-fokosun <okosunuzflorence@gmail.com>
2015-10-16 13:50:12 +0100

    Updated tests and travis.yml file

--- tests/DictionaryTest.php
+++ tests/DictionaryTest.php
@@ -35,8 +35,78 @@ class DictionaryTest extends \PHPUnit_Framework_TestCase
 
         $this->assertNotEmpty($stack);
 
+        // return $stack;
+
     }
 
+    /** test three
+    * tests for findSlang
+    */
+
+    public function testFindSlang()
+    {
+
+        $this->dictionary->addSlang('bar', 'Term for money', 'Hold your bar');
+
+        $wordMatch = $this->dictionary->findSlang('bar');
+
+        $this->assertEquals('bar', $wordMatch['slang']);
+
+        $this->assertEquals('Term for money', $wordMatch['description']);
+
+        $this->assertEquals('Hold your bar', $wordMatch['sample-sentence']);
+
+    }
+
+    /** test four
+    * tests for updateSlang
+    */
+
+    public function testUpdateSlang()
+    {
+
+        $this->dictionary->addSlang('bar', 'Term for money', 'Hold your bar');
+
+        $this->dictionary->updateSlang('bar', 'A colloquail term for money', 'Hold your bar');
+
+        $this->assertEquals('A colloquail term for money', $this->dictionary->getData()['bar']['description']);
+
+        $this->assertEquals('Hold your bar', $this->dictionary->getData()['bar']['sample-sentence']);
+    }
+
+    /** test five
+    *
+    * This one tests removeSlang
+    */
+
+    public function testRemoveSlang()
+    {
+
+        $this->dictionary->addSlang('bar', 'Term for money', 'Hold your bar');
+
+        $this->dictionary->removeSlang('bar');
+
+        $this->assertFalse(isset($this->dictionary->getData()['bar']));
+
+    }
+
+    /** test six
+    *
+    * Ranks test
+    */
+
+    public function testRankWords()
+    {
+
+        $word = $this->dictionary->rankWords("Prosper, have you finished the curriculum? Yes, tight tight tight");
+
+        foreach( $word as $key => $value )
+        {
+            $this->assertArrayHasKey($key, $word);
+            $this->assertEquals($value, $word[$key]);
+        }
+
+    }
 
 
     public function dataSource()
@@ -47,6 +117,4 @@ class DictionaryTest extends \PHPUnit_Framework_TestCase
 
         );
     }
-
-
 }
