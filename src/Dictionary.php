fc873de10617f3260b48d4e322b4ce15506cedda
andela-fokosun <okosunuzflorence@gmail.com>
2015-10-22 17:25:01 +0100

    Modified the Dictionary classes

--- src/Dictionary.php
+++ src/Dictionary.php
@@ -2,188 +2,91 @@
 
 namespace Florence;
 
-class Dictionary
-{
+use Florence\Data;
+
+class Dictionary {
 
     private $data;
 
-    /**
-    * Initialize the constructor
-    */
-
-    public function __construct()
-    {
-
+    public function __construct() {
         $this->data = Data::$data;
-
     }
 
-    /**
-    * Set Data
-    */
-
-    public function setData( $data )
-    {
-
-        $this->data = $data;
-
-    }
-
-    /**
-    * Return the data
-    */
-
-    public function getData()
-    {
-
+    public function getData() {
         return $this->data;
-
     }
 
-    /**
-    * Adds a slang, it's description and sample sentence to the dictionary array in the
-    * Data class.
-    */
+    public function addSlangToDictionary($slang, $meaning, $sentence) {
 
-    public function addSlang( $slang, $description, $sentence )
-    {
-
-        /* Quick check if the slang exists in
-        * the dictionary already
-        * if yes, throw WordExistsException
-        * else add the slang
-        */
-
-        if(! array_key_exists($slang, $this->data))
-        {
+        if(! array_key_exists($slang, $this->data)) {
             $this->data[$slang] = [
                 'slang' => $slang,
-                'description' => $description,
+                'description' => $meaning,
                 'sample-sentence' => $sentence
             ];
-        }
-        else
-        {
+        } else {
             throw new WordExistsException( $slang . ' already exists in the dictionary' );
         }
 
     }
 
-    /**
-    * Updates the description and sample sentence of a word in the dictionary array of the
-    * Data class.
-    */
+    public function deleteSlangFromDictionary($slang) {
 
-    public function updateSlang( $slang, $description, $sentence )
-    {
+        if(array_key_exists($slang, $this->data)) {
 
-        $data = array();
-
-        if( $this->arrayKeyExists($slang) )
-        {
-            $data['description'] = $description;
-            $data['sample-sentence'] = $sentence;
-            $this->data[$slang] = $data;
-        }
-        else
-        {
-            throw new WordNotFoundException($slang . ' not found in the dictionary');
-        }
-    }
-
-    /**
-    * Get the full detail of a slang that is added to the dictionary array
-    */
-
-    public function findSlang( $slang )
-    {
-
-        if( $this->arrayKeyExists($slang))
-        {
-            return $this->data[$slang];
-        }
-        else
-        {
-            throw new WordNotFoundException( $slang . ' not found in the dictionary' );
-        }
-
-    }
-
-    public function arrayKeyExists($slang)
-    {
-        if( array_key_exists( $slang, $this->data) )
-            return true;
-    }
-
-    /**
-    * Remove a slang and it's attributes from the dictionary array
-    */
-
-    public function removeSlang( $slang )
-    {
-
-        if( array_key_exists( $slang, $this->data) )
-        {
             unset ( $this->data[$slang] );
+
         }
-        else
-        {
-            throw new WordNotFoundException( $slang . ' not found in the dictionary' );
+
+        } else {
+
+            throw new WordNotFoundException( $slang . 'word not found in the dictionary' );
         }
 
     }
 
+    public function updateExistingSlang($slang, $meaning, $sentence) {
 
-    /**
-    * Group the same words according to the number of occurrences in a given sentence.
-    */
+        if(array_key_exists($slang, $this->data)) {
 
-    public function rankWords( $sentence )
-    {
-        /* $words is returned as an array*/
+            $this->data[$slang] = [
+                'slang' => $slang,
+                'meaning' => $meaning,
+                'sentence' => $sentence
+            ];
 
-        $words = str_word_count( $sentence, 1 );
-        $ranking = [];
+        } else {
 
-        /* traverse through the $words array */
-
-        foreach( $words as $word )
-        {
-            if( isset($ranking[strtolower( $word )]) )
-            {
-                $ranking[strtolower( $word )]++;
-            }
-            else
-            {
-                $ranking [strtolower( $word )] = 1;
-            }
+            throw new WordNotFoundException( $slang . 'word not found in the dictionary' );
         }
+    }
 
-        $this->sortWords($ranking);
+    public function findAndRetrieveSlang( $slang ) {
 
-        return $ranking;
+        if( array_key_exists($slang, $this->data) ) {
+            return $this->data[$slang];
+        } else {
+            throw new WordNotFoundException( $slang . ' word not found in the dictionary' );
+        }
 
     }
 
-    private function sortWords( $ranking )
-    {
+    public function rankAndSort($sentence) {
 
-        if(! is_array( $ranking ))
-        {
-            throw new InvalidArgumentException("The method argument must be of type array");
+        $unsorted = [];
+        $sorted = [];
+
+        $unsorted = (array_count_values(str_word_count(strtolower($sentence),1)));
+
+        arsort($unsorted);
+
+        foreach($unsorted as $key => $val) {
+            $word = $key;
+            $count = $val;
+            array_push($sorted,[$word => $count]);
         }
 
-        foreach( $ranking as $key => $value )
-        {
-            $tag = array();
-            $count = array();
-
-            $tag [] = $key;
-            $count [] = $value;
-        }
-
-        array_multisort($ranking );
-
+        return $sorted;
     }
 
 }
